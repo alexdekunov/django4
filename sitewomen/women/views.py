ba5ac732b8b4539d4195a075746349d7b4108693
alexdekunov <wresda@gmail.com>
2023-12-10 23:39:59 +0300

    2

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -2,9 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def index(request): # HttpRequest
+def index(request):  # HttpRequest
     return HttpResponse("Страница приложения women")
 
 
-def categories(request): # HttpRequest
-    return HttpResponse("<h1>Статьи по категориям</h1> ")
+def categories(request, cat_id):  # HttpRequest
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+
+
+def archive(request, year):
+    return HttpResponse(f"Архив по годам<p>{year}</p>")

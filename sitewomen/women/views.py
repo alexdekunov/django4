5d2cfb77dd23c1cb595d5a167c26dd05d60ad07f
Aleksandr Dekunov <wresda@gmail.com>
2023-12-11 17:47:27 +0300

    2

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render
 
 
@@ -11,8 +11,18 @@ def categories(request, cat_id):  # HttpRequest
 
 
 def categories_by_slug(request, cat_slug):
+    if request.POST:
+        print(request.POST)
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
 
 
 def archive(request, year):
+    if year > 2023:
+        raise Http404()
+
     return HttpResponse(f"Архив по годам<p>{year}</p>")
+
+
+def page_not_found(requests, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
+
